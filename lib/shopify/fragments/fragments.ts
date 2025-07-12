b5dbc6daac5d16ab0ef063812685a1613b756bf5
Victor Wafula <56592093+Wafulah@users.noreply.github.com>
2025-07-12 11:41:48 +0300

    Create fragments.ts

---
+++ lib/shopify/fragments/fragments.ts
@@ -0,0 +1,37 @@
+// interfaces for Appwrite responses
+
+export interface AppImage {
+  $id: string;
+  url: string;
+  altText?: string;
+}
+
+export interface AppSeo {
+  title?: string;
+  description?: string;
+}
+
+export interface AppProduct {
+  $id: string;
+  name: string;
+  slug: string;
+  description: string;
+  image: AppImage;
+  images?: AppImage[];
+  price: number;
+  seo?: AppSeo;
+}
+
+export interface CartItem {
+  $id: string;
+  product: AppProduct;
+  quantity: number;
+}
+
+export interface AppCart {
+  $id: string;
+  userId?: string;
+  items: CartItem[];
+  createdAt: string;
+  updatedAt: string;
+}
