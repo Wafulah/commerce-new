3b5f362b9191dcd4c86aa7052a0f4cfa674a7b43
Victor Wafula <56592093+Wafulah@users.noreply.github.com>
2025-07-16 08:28:45 +0300

    Update product.ts

--- lib/appwrite/queries/product.ts
+++ lib/appwrite/queries/product.ts
@@ -1,5 +1,5 @@
 import { databases, Query } from '@/lib/appwrite';
-import { shapeProduct } from '../product';
+import { shapeProduct } from '../fragments/product';
 import type { AppProduct } from '../fragments';
 
 const DATABASE_ID          = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
