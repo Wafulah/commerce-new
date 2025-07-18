6eef8ad130c4d975304c9642a28bc6afc25cc229
Victor Wafula <56592093+Wafulah@users.noreply.github.com>
2025-07-18 09:06:04 +0300

    Update cart.ts

--- lib/appwrite/queries/cart.ts
+++ lib/appwrite/queries/cart.ts
@@ -1,4 +1,4 @@
-import { createAdminClient } from "@/lib/appwrite";
+import { createAdminClient } from "@/lib/appwrite/appwrite";
 import { shapeCart } from '../fragments/cart';
 import type { AppCart } from '../fragments/fragments';
 
